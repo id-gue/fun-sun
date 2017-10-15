8dae201eb35b12156cf852abef40cf6284ac37d2
Inna Janssen <janssen@jast-it.de>
2017-10-15 10:36:16 +0200

    Work in progress. displays list from json output

--- php/sun.php
+++ php/sun.php
@@ -5,32 +5,64 @@
  * Date: 14.10.2017
  * Time: 17:45
  */
-
-define (PYTHON_API,"/usr/bin/python sun.py --lat {lat} --lon {lon} --distance {distance}");
-
+require_once 'Poi.php';
+//>test.json 2>&1
+define('PYTHON_API', "/usr/bin/python3 /var/www/funsun/play.py --lat {lat} --lon {lon} --distance {distance}");
+error_reporting(E_ALL);
+ini_set('display_errors', true);
 $file = file_get_contents('template.html');
-$results="";
-if (count($_POST)>0)
-{
-    var_dump($_POST);
-    $lat=$_POST['lat'];
-    $lon=$_POST['lon'];
+
+$formattedResults = "";
+if (count($_POST) > 0) {
+    //var_dump($_POST);
+    $lat = $_POST['lat'];
+    $lon = $_POST['lon'];
     if (!is_numeric($lat)) exit;
     if (!is_numeric($lon)) exit;
 
-    if (isset($_POST['distance1'])) $distance=100;
-    else $distance=500;
+    if (isset($_POST['distance1'])) $distance = 10;
+    else $distance = 50;
 
-    $toPython=PYTHON_API;
-    $toPython = str_replace('{lat}',$lat,$toPython);
-    $toPython = str_replace('{lon}',$lon,$toPython);
-    $toPython = str_replace('{distance}',$distance,$toPython);
-echo $toPython;
-    exec(PYTHON_API,$result);
-    $json= json_decode($result);
+    $toPython = PYTHON_API;
+    $toPython = str_replace('{lat}', $lat, $toPython);
+    $toPython = str_replace('{lon}', $lon, $toPython);
+    $toPython = str_replace('{distance}', $distance, $toPython);
+/*
+    echo "<br>".$toPython . "<br>";
+    $result = exec($toPython, $returnState);
+  */
+    //temporary
+    $result = file_get_contents("/var/www/funsun/output.json");
+
+    if (isset($result)) {
+        $json = json_decode($result, true);
+        //var_dump($json);
+    }
+
+    $pois = array();
+
+    //Put JSON into Object
+    foreach ($json as $res) {
+        //echo "<hr>";
+        //print_r($res);
+        $poi = new Poi();
+        $poi->title = $res['title'];
+        $poi->lat = $res['location']['latitude'];
+        $poi->lon = $res['location']['longitude'];
+        $pois[] = $poi;
+    }
+
+    //create a nice list
+    $formattedResults = "<ul>";
+    foreach ($pois as $poi) {
+        $formattedResults .= "<li>" . $poi->title . "</li>";
+    }
+
+    $formattedResults .= "</ul>";
 }
 
-$file = str_replace('{results}',print_r($result,true),$file);
+//output the template
+$file = str_replace('{results}', $formattedResults, $file);
 
 echo $file;
 
